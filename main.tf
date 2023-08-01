2eb418761fe0405b5a2b1cdd088ade302be4bb8f
Jonathan Reed <jonree@microsoft.com>
2023-08-01 10:50:38 -0400

    update vm with current syntax, add password generator

--- main.tf
+++ main.tf
@@ -51,27 +51,37 @@ address_prefixes     = ["10.0.4.0/24"]
 
 }
 
-resource "azurerm_virtual_machine" "main" {
+resource "azurerm_windows_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
+  size                  = "Standard_DS1_v2"
+  admin_username        = "adminuser"
+  admin_password        = random_password.password.result
   
-  storage_image_reference {
+  source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
     sku       = "2022-datacenter-azure-edition"
     version   = "latest"
   }
  
-  storage_os_disk {
+  os_disk {
     name                 = "${var.prefix}-vmOsDisk"
     caching              = "ReadWrite"
-    create_option        = "FromImage"
-    managed_disk_type    = "Standard_LRS"
+    storage_account_type = "Standard_LRS"
   }
 
+}
+
+resource "random_password" "password" {
+  length      = 20
+  min_lower   = 1
+  min_upper   = 1
+  min_numeric = 1
+  min_special = 1
+  special     = true
 }
 
 
